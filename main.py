f94962031eea72aec57ff53024eb621cb475bc06
wrottger <wrottger@student.42wolfsburg.de>
2023-09-19 14:04:23 +0200

    Updated layout

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from collections import deque
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', "https://fonts.googleapis.com/css?family=Noto+Sans"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="City Data Dashboard", update_title=False)
 
 app.layout = html.Div([
     html.H3('City Data Dashboard'),
@@ -49,12 +49,15 @@ def display_area(y):
         domain={'x': [0, 1], 'y': [0, 1]},
         title={'text': "Lärmpegel"},
         gauge={'axis': {'range': [None, 100]},
-               'bar': {'color': "darkblue"},
+               'bar': {'color': "rgb(25, 55, 89)"},
                'steps': [
-                   {'range': [0, 65], 'color': "green"},
+                   {'range': [0, 65], 'color': "rgb(193,205,13)"},
                    {'range': [65, 80], 'color': "orange"},
                    {'range': [80, 100], 'color': "red"}]}
     ))
+    fig.update_layout(
+        margin=dict(l=40, r=40)
+    )
     return fig
 
 
@@ -63,10 +66,21 @@ def display_area(y):
     Input("graph-interval", "n_intervals"))
 def display_area(y):
     with open("data/loudness", 'r') as f:
-        decibel = int(float(f.readline()))
+        decibel = int(float(f.readline())) / 100
     historic_sound.append(decibel)
-    fig = px.area(list(historic_sound), labels=None, title='<br>          Historischer Lärmpegel')
-    fig.update_layout(showlegend=False, yaxis_title=None, xaxis_title=None)
+    fig = px.line(list(historic_sound), labels=None, title='<br>          Historischer Lärmpegel')
+    fig.update_layout(
+        margin=dict(l=20, r=10),
+        showlegend=False,
+        yaxis_title=None,
+        xaxis_title=None,
+        yaxis_tickformat=',.0%',
+        plot_bgcolor='rgb(255,255,255)')
+    fig.update_yaxes(
+        showgrid=True,
+        range=[0, 1],
+        gridcolor="rgb(240,240,240)")
+    fig.update_traces(line_color='rgb(25, 55, 89)')
     return fig
 
 
@@ -77,8 +91,21 @@ def display_area(y):
     with open("data/traffic_volume", 'r') as f:
         traffic = int(float(f.readline()))
     historic_traffic.append(traffic)
-    fig = px.area(list(historic_traffic), labels=None, title='<br>          Verkehrsaufkommen')
-    fig.update_layout(showlegend=False, yaxis_title=None, xaxis_title=None)
+    fig = px.line(list(historic_traffic), labels=None, title='<br>          Verkehrsaufkommen')
+    fig.update_layout(
+        margin=dict(l=20, r=10),
+        showlegend=False,
+        yaxis_title=None,
+        xaxis_title=None,
+        plot_bgcolor='rgb(255,255,255)'
+    )
+    fig.update_yaxes(
+        showgrid=True,
+        range=[0, 6],
+        gridcolor="rgb(240,240,240)"
+    )
+    fig.update_xaxes(showgrid=False)
+    fig.update_traces(line_color='rgb(25, 55, 89)')
     return fig
 
 
